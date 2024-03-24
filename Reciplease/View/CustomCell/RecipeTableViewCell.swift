12bd81ee41053713a4710c8a59c003edaa662a19
xNumero3 <notnumero3@gmail.com>
2024-03-24 18:30:27 +0100

    adding base view

---
+++ Reciplease/View/CustomCell/RecipeTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeTableViewCell.swift
+//  Reciplease
+//
+//  Created by François-Xavier on 19/03/2024.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+        
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    
+    static let identifier = "RecipeCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "RecipeTableViewCell", bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
