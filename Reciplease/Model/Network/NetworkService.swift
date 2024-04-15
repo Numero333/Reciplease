5e0146734a4be7a0005858a5a6aa7080e77e0182
xNumero3 <notnumero3@gmail.com>
2024-04-15 14:51:51 +0200

    add dependency injection

--- Reciplease/Model/Network/NetworkService.swift
+++ Reciplease/Model/Network/NetworkService.swift
@@ -8,7 +8,17 @@
 import Foundation
 import Alamofire
 
-final class NetworkService {
+protocol AlamofireSessionProtocol {
+    func performRequest<T: Decodable>(apiRequest: APIConfiguration) async throws -> T
+}
+
+final class NetworkService: AlamofireSessionProtocol {
+    
+    let session: Session
+    
+    init(session: Session = Session()){
+        self.session = session
+    }
             
     func performRequest<T: Decodable>(apiRequest: APIConfiguration) async throws -> T {
         return try await AF.request(apiRequest.url.value, method: apiRequest.method, parameters: apiRequest.parameters.value)
