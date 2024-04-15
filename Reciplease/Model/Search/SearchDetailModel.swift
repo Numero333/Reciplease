a33ab8a2b74fc849d6d793c38a0c26df7755e14e
xNumero3 <notnumero3@gmail.com>
2024-04-15 15:46:23 +0200

    refactoring

--- Reciplease/Model/Search/SearchDetailModel.swift
+++ Reciplease/Model/Search/SearchDetailModel.swift
@@ -59,7 +59,6 @@ final class SearchDetailModel {
         }
     }
     
-<<<<<<< Updated upstream
     private func configureRecipeEntity(for newRecipe: RecipeEntity, recipe: RecipeDescription) {
         newRecipe.label = recipe.label
         newRecipe.ingredients = recipe.ingredientLines.joined(separator: " ")
@@ -68,100 +67,4 @@ final class SearchDetailModel {
         newRecipe.url = recipe.url
         newRecipe.yield = Int16(recipe.yield)
     }
-=======
-//    private func configureRecipeEntity(for newRecipe: RecipeEntity, recipe: RecipeDescription) {
-//        newRecipe.label = recipe.label
-//        newRecipe.ingredients = recipe.ingredientLines.joined(separator: " ")
-//        newRecipe.duration = recipe.durationFormatted
-//        newRecipe.image = recipe.image
-//        newRecipe.url = recipe.url
-//        newRecipe.yield = Int16(recipe.yield)
-//    }
->>>>>>> Stashed changes
 }
-
-
-
-
-
-
-
-
-
-
-////
-////  SearchDetailModel.swift
-////  Reciplease
-////
-////  Created by François-Xavier on 27/03/2024.
-////
-//
-//import Foundation
-//import CoreData
-//
-//protocol SearchDetailDelegate: AnyObject {
-//    func didUpdate(liked: Bool)
-//}
-//
-//final class SearchDetailModel {
-//
-//    //MARK: - Property
-//    private let coreDataService = CoreDataService.shared
-//    private var likeState = false
-//    private var recipe = [RecipeEntity]()
-//    var selectedRecipe: RecipeDescription
-//    weak var delegate: SearchDetailDelegate?
-//
-//    //MARK: - Initialization
-//    init(selectedRecipe: RecipeDescription) {
-//        self.selectedRecipe = selectedRecipe
-//    }
-//
-//    //MARK: - Accessible
-//    func loadData() {
-//        Task {
-//            let predicate = NSPredicate(format: "label == %@", selectedRecipe.label)
-//            recipe = await self.coreDataService.read(entityType: RecipeEntity.self, context: self.coreDataService.mainContext, predicate: predicate, sortDescriptors: nil)
-//            if !recipe.isEmpty {
-//                delegate?.didUpdate(liked: true)
-//                likeState.toggle()
-//            }
-//        }
-//    }
-//
-//    func saveRecipeToDatabase() {
-//        let context = coreDataService.mainContext
-//        let newRecipe = RecipeEntity(context: context)
-//        configureRecipeEntity(for: newRecipe, recipe: selectedRecipe)
-//        Task {
-//            await self.coreDataService.save(context: context)
-//        }
-//        loadData()
-//    }
-//
-//    func handleFavoriteButton() {
-//        likeState ? deleteRecipeFromDatabase() : saveRecipeToDatabase()
-//    }
-//
-//    //MARK: - Private
-//    private func deleteRecipeFromDatabase() {
-//        Task {
-//            await self.coreDataService.delete(objects: recipe, context: self.coreDataService.mainContext)
-//            await self.coreDataService.save(context: self.coreDataService.mainContext)
-//            recipe = []
-//            if recipe.isEmpty {
-//                delegate?.didUpdate(liked: false)
-//                likeState.toggle()
-//            }
-//        }
-//    }
-//
-//    private func configureRecipeEntity(for newRecipe: RecipeEntity, recipe: RecipeDescription) {
-//        newRecipe.label = recipe.label
-//        newRecipe.ingredients = recipe.ingredientLines.joined(separator: " ")
-//        newRecipe.duration = recipe.durationFormatted
-//        newRecipe.image = recipe.image
-//        newRecipe.url = recipe.url
-//        newRecipe.yield = Int16(recipe.yield)
-//    }
-//    }
