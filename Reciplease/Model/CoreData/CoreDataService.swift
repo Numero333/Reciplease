0888b3d13b7f86aa2c063289019e6334023adb02
xNumero3 <notnumero3@gmail.com>
2024-04-16 16:12:57 +0200

    refact

--- Reciplease/Model/CoreData/CoreDataService.swift
+++ Reciplease/Model/CoreData/CoreDataService.swift
@@ -4,6 +4,7 @@
 //
 //  Created by François-Xavier on 28/03/2024.
 //
+
 import CoreData
 
 protocol CoreDataServiceInterface: AnyObject {
@@ -42,31 +43,28 @@ final class CoreDataService: CoreDataServiceInterface {
     }
     
     // MARK: - Accessible
-    func save(context: NSManagedObjectContext) async throws {
-        try await context.perform {
+    func read<T: NSManagedObject>(entityType: T.Type, context: NSManagedObjectContext, predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?) async -> [T] {
+    return await context.perform {
+        let request: NSFetchRequest<T> = NSFetchRequest(entityName: String(describing: entityType))
+        request.predicate = predicate
+        request.sortDescriptors = sortDescriptors
+        do {
+            return try context.fetch(request)
+        } catch {
+            print("An error occurred during the reading operation: \(error)")
+            return []
+        }
+    }
+}
+    func save(context: NSManagedObjectContext) async {
+        await context.perform {
             do {
                 try context.save()
             } catch {
-                print(error)
-                throw error
+                print("An error occurred during the saving operation: \(error)")
             }
         }
     }
-    
-    func read<T: NSManagedObject>(entityType: T.Type, context: NSManagedObjectContext, predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?) async -> [T] {
-        return try await context.perform {
-            let request: NSFetchRequest<T> = NSFetchRequest(entityName: String(describing: entityType))
-            request.predicate = predicate
-            request.sortDescriptors = sortDescriptors
-            do {
-                return try context.fetch(request)
-            } catch {
-                print("An error occurred during the reading operation: \(error)")
-                return []
-            }
-        }
-    }
-    
     func delete(objects: [NSManagedObject], context: NSManagedObjectContext) async {
         await context.perform {
             objects.forEach { context.delete($0) }
