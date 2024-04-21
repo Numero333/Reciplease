2e66552e4c0a540688e9ed27ff194c61207b0921
xNumero3 <notnumero3@gmail.com>
2024-04-21 16:56:21 +0200

    add comments

--- RecipleaseTests/Network/SessionFakeProtocol.swift
+++ RecipleaseTests/Network/SessionFakeProtocol.swift
@@ -20,24 +20,30 @@ class SessionFakeProtocol: URLProtocol {
     }
     
     override func startLoading() {
+        // Retrieve the handler that provides the response and data
         let handler = SessionFakeProtocol.loadingData!
+        
+        // Execute the handler to get the simulated response and data
         let (response, data) = handler()
         
+        // Ensure a response is available otherwise notify the client that loading has finished
         guard let response = response else {
             client?.urlProtocolDidFinishLoading(self)
             return
         }
         
+        // Notify the client that a response has been received
         client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
         
+        // If data is available, send it to the client
         if let data = data {
             client?.urlProtocol(self, didLoad: data)
         }
+        
+        // Notify the client that loading has finished
         client?.urlProtocolDidFinishLoading(self)
     }
     
-    override func stopLoading() {
-        
-    }
-    
+    // Stops loading the request
+    override func stopLoading() { }
 }
